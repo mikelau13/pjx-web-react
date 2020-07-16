ae18003cf3d7bb70392b2e0f30392d360a481ded
mikelau13 <mikelau13@hotmail.com>
2020-07-16 11:38:37 -0400

    initial commit - react.js

---
+++ build/precache-manifest.442fefe7fcdcc6736a948c64d4cd1b02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496f2ee94652b6239f17466e313500ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9fffc15e1e8d7679273",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3643a1db60496146fd37",
+    "url": "/static/js/2.362ad7c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.362ad7c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9fffc15e1e8d7679273",
+    "url": "/static/js/main.42fa5fbe.chunk.js"
+  },
+  {
+    "revision": "3b5a8c33a48ed019c3f8",
+    "url": "/static/js/runtime-main.6468af3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
