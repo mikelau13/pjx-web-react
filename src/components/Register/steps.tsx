d41bb583ae960f3a9c27455da0ce0bec66460701
mikelau13 <mikelau13@hotmail.com>
2020-08-05 14:58:50 -0400

    steppers css clean up

--- src/components/Register/steps.tsx
+++ src/components/Register/steps.tsx
@@ -7,14 +7,8 @@ import StepLabel from '@material-ui/core/StepLabel';
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-  },
-  button: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
+    margin: '10px'
+  }
 }));
 
 function getSteps() {
